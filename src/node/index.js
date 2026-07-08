e67b9fbdc77117ac8cf90380110939d557cdad81
ren.yamaguchi <re-yamaguchi@brexa.com>
2026-07-08 11:39:34 +0900

    dev-marathon pass 12km

--- src/node/index.js
+++ src/node/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const port = 社員番号;
+const port = 5921;
 
 const cors = require("cors");
 app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "x",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_5921", // PostgreSQLのユーザー名に置き換えてください
+  host: "postgres", // PostgreSQLのホスト名に置き換えてください
+  database: "crm_5921", // PostgreSQLのデータベース名に置き換えてください
+  password: "pass_5921", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -37,10 +37,36 @@ app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.put("/customers/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { companyName, industry, contact, location } = req.body;
+    const updatedCustomer = await pool.query(
+      "UPDATE customers SET company_name = $1, industry = $2, contact = $3, location = $4 WHERE customer_id = $5 RETURNING *",
+      [companyName, industry, contact, location, id]
+    );
+    res.json({ success: true, customer: updatedCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.delete("/customers/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM customers WHERE customer_id = $1", [id]);
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
     res.json({ success: false });
